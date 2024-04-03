e3a23eeda754e66fe9fe661aede65204570b7b1e
Vedesh Padal <vpadal04@gmail.com>
2024-04-03 15:48:51 +0530

    server code complete
    - basic server code is complete, yet to add authentication and other
    functionality
    - yet to implement full functionality like:
    	- authentication
    	- pagination/search
    	- comments on the posts

---
+++ controllers/posts.js
@@ -0,0 +1,62 @@
+import PostMessage from '../models/postMessage.js'
+import mongoose from 'mongoose';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    }   catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+
+};
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    console.log(post);
+    const newPost = new PostMessage(post)
+
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    }   catch (error)   {
+        res.status(409).json({message: error.message })
+    }
+
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))  return res.status(404).send('No post with that ID');
+
+    // new: true is to receive the updated version of the post
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(id))  return res.status(404).send('No post with that ID');
+
+    await PostMessage.findOneAndDelete({ _id: id });
+
+    res.json({ message: 'Post deleted successfully!' });
+
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(id))  return res.status(404).send('No post with that ID');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    res.json(updatedPost);
+}
--- index.js
+++ index.js
@@ -2,3 +2,30 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from  'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+// configuring bodyParser so that the client can properly send the requests
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use('/posts', postRoutes)
+
+
+
+// connecting to db (mongoose is being used here)
+const PORT = process.env.PORT || 5000;
+
+// useNewUrlParser: true indicating the use of the new connection string parser and useUnifiedTopology: true enabling the new connection management engine provided by the MongoDB driver.
+mongoose.connect(process.env.CONNECTION_URL)   // the 2nd param as an object is for minimizing errors/warnings in the console, and this is deprecated: useUnifiedApology: true
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+
+// mongoose.set('useFindAndModify', false);    // to make sure that we don't have warnings in the console
+
